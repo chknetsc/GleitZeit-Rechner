1986b88e15cfd8e45b1b726967324f8ba1ea5199
knetch01 <knetch01@LN090018D1.HQ001.CORP.SFY>
2013-07-25 13:53:25 +0200

    Rechner für Zeit Funktioniert Komplett Nur DateFormat kann noch verbessert werden 0100

--- GleitZeitRechner/src/com/chknetsc/gleitzeitrechner/RechnerActivity.java
+++ GleitZeitRechner/src/com/chknetsc/gleitzeitrechner/RechnerActivity.java
@@ -1,9 +1,15 @@
 package com.chknetsc.gleitzeitrechner;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -65,30 +71,54 @@ public class RechnerActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				if(calcready) {
-					// Werte prüfen
-					int ende = 0;
-					int start = 0;
-					int brea = 0;
+					// Values Intit
+					long gesamt = 0;
+					int min = 0;
+					int hour = 0;
 					
-					// Check if Text input is Empty
-					if(!endTime.getText().toString().isEmpty()) {
-						ende = Integer.valueOf(endTime.getText().toString());
-					}
-					if(!startTime.getText().toString().isEmpty()) {
-						start = Integer.valueOf(startTime.getText().toString());
-					}
-					if(!breakTime.getText().toString().isEmpty()) {
-						brea = Integer.valueOf(breakTime.getText().toString());
+					SimpleDateFormat df = new SimpleDateFormat("HHmm");
+					Date breakD = null;
+					Date startD = null;
+					Date endD = null;
+					
+					try {
+						// Check if Text Field is empty an fill it with a Time
+						if(!startTime.getText().toString().isEmpty()) {
+							startD = df.parse(startTime.getText().toString());
+						} else {
+							startD = df.parse("0000");
+						}
+						log("startD = " + startD);
+						
+						if(!endTime.getText().toString().isEmpty()) {
+							endD = df.parse(endTime.getText().toString());
+						} else {
+							endD = df.parse("0000");
+						}
+						log("endD = " + endD);
+						
+						if(!breakTime.getText().toString().isEmpty()) {
+							breakD = df.parse(breakTime.getText().toString());
+						} else {
+							breakD = df.parse("0000");
+						}
+						log("breakD = " + breakD);
+					
+					} catch (ParseException e) {
+						Log.e(TAG, "Fehler Zeitumwandlung nicht möglich", e);
+						e.printStackTrace();
 					}
 					
-					log("Ende Zeit: " + ende);
-					log("start Zeit: " + start);
-					log("brea Zeit: " + brea);
+					// Calc Total Working Time and convert it in Hour and Minutes
+					gesamt = (endD.getTime() - startD.getTime()) - breakD.getTime();
+					log("Gesamt = " + gesamt + "ms");
+					int days = (int) (gesamt / (1000*60*60*24));
+					hour = (int) ((gesamt - (1000*60*60*24*days)) / (1000*60*60));
+					min = (int) (gesamt - (1000*60*60*24*days) - (1000*60*60*hour)) / (1000*60);
+					log("Gesamt = " + hour + "Stunden");
+					log("Gesamt = " + min + "min");
 					
-					// Gesamtzeitberechne
-					ende = (ende - start) - brea;
-					// Ausgabe Gesammtzeit
-					result.setText("Gesamtzeit: " + ende);
+					result.setText("Gesamtzeit: " + hour + ":" + min);
 				}
 			}
 		});
